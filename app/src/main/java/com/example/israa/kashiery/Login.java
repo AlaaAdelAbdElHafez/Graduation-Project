b8fd67bccd8885e9e2b838399766c0bfd6ec2dc8
Esraagamal2 <esraagamal506@gmail.com>
2017-09-13 21:05:36 +0200

    Graduation-Project

---
+++ app/src/main/java/com/example/israa/kashiery/Login.java
@@ -0,0 +1,16 @@
+package com.example.israa.kashiery;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Israa on 13/09/2017.
+ */
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
